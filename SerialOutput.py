16eeab945072d3b98173fc32e189f8d4450e5321
BahShaman <mikeboscia@gmail.com>
2014-10-13 23:11:57 -0700

    change to using array and sending all as one piece

--- SerialOutput.py
+++ SerialOutput.py
@@ -3,6 +3,7 @@ import serial
 import serial.tools.list_ports
 import time
 
+"""
 i=0
 try:
 	ser = serial.Serial(None, 9600, timeout=1)
@@ -13,12 +14,15 @@ try:
 except serial.SerialException:
 	pass
 i+=1
+"""
 
-
+"""
 try:
 	port = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=3.0)
 except serial.SerialException:
 	print "hello"
+"""
+
 	
 ports = list(serial.tools.list_ports.comports())
 print "%d ports found" % len(ports)
@@ -29,7 +33,70 @@ for port in ports:
 
 	
 print serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE
-	
+"""
+if False:	
+	#try:
+	wport = serial.Serial('COM4', baudrate=38400, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=3.0)
+	#wport.open()
+	print wport
+	wport.write("hello")
+	print "starting"
+	st = ""
+	while True:
+		#print "printing %s" % 0
+		for x in range(8):
+			time.sleep(2)
+			for i in range(8):
+				if x % 3 == i % 3:
+					my_int = 255
+				else:
+					my_int = 0
+				st += str(" {:d}".format(i % 3)) + "=" + str("{:d}".format(x % 3))
+				print " {:3d}".format(my_int) ,
+				print len(bytes(chr(my_int)))
+				wport.write(bytes(chr(my_int)))
+			wport.write(chr(13))
+			#print wport.readline()
+			print st
+			st = ""
+			x = raw_input("pausing")
+	while True:
+		print wport.read(1),
+	#except serial.SerialException:
+	#	print "error reading from port %s" % wport.name
+"""		
+
+
+if True:	
+	#try:
+	wport = serial.Serial('COM4', baudrate=38400, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=3.0)
+	print wport
+	print "starting"
+	while True:
+		for x in range(8):
+			time.sleep(2)
+			sendbytes = []
+			for i in range(8):
+				if x == i:
+				#if x % 1 == i % 1:
+					my_int = 255
+				else:
+					my_int = 0
+				sendbytes.append(bytes(chr(my_int)),)
+				sendbytes.append("!")
+				print " {:3d}".format(my_int) ,
+				#print len(bytes(chr(my_int)),) ,
+			wport.write(sendbytes)
+			print len(sendbytes)
+			z = raw_input("pausing")
+	while True:
+		print wport.read(1),
+	#except serial.SerialException:
+	#	print "error reading from port %s" % wport.name
+
+
+
+"""
 #try:
 wport = serial.Serial('COM4', baudrate=38400, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=3.0)
 #wport.open()
@@ -48,16 +115,17 @@ while True:
 				my_int = 0
 			st += str(" {:d}".format(i % 3)) + "=" + str("{:d}".format(x % 3))
 			print " {:3d}".format(my_int) ,
+			print len(bytes(chr(my_int)))
 			wport.write(bytes(chr(my_int)))
-		#wport.write(chr(13))
+		wport.write(chr(13))
 		#print wport.readline()
 		print st
 		st = ""
+		x = raw_input("pausing")
 while True:
 	print wport.read(1),
 #except serial.SerialException:
 #	print "error reading from port %s" % wport.name
-		
-		
+"""		
 		
 		
