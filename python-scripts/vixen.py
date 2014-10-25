025194ccabff9ddae877f40287f6b2ad6bbb0645
BahShaman <mikeboscia@gmail.com>
2014-10-25 15:10:22 -0700

    file reading and finding updates. New function to return an array of the period as integers

--- python-scripts/vixen.py
+++ python-scripts/vixen.py
@@ -9,6 +9,10 @@ class Vixen(object):
 	has_musicfile = False
 	has_vixfile = False
 	has_profilefile = False
+	basedirpath = ''
+	defaultseqdir = 'Sequences/'
+	defaultmusdir = 'Audio/'
+	defaultprodir = 'Profiles/'
 	vixfilename = ''
 	musicfilename = ''
 	channels = 1
@@ -26,8 +30,11 @@ class Vixen(object):
 		#os.environ['SDL_VIDEODRIVER']="fbcon" # could be "directfb", try "aalib" for extra fun
 	
 	def __del__(self):
-		pass
-
+		try:
+			pygame.mixer.music.stop()
+		except:
+			pass
+	
 	def set_screen(self,size):
 		self.screen = pygame.display.set_mode(size)
 		return self.screen
@@ -40,55 +47,75 @@ class Vixen(object):
 		self.loadvixfile(filename)
 	
 	def loadvixfile(self,vixfilename):
-		"""---"""				
-		if not os.path.exists(vixfilename):
-			raise "File %s cannot be found" % vixfilename
+		"""---"""
+		if self.basedirpath != '':
+			print "Using base dir path %s" % self.basedirpath
+			checkfilepath = self.basedirpath + '/' + self.defaultseqdir + vixfilename
 		else:
+			checkfilepath = vixfilename
+		print "loading file %s" % checkfilepath
+		if not os.path.exists(checkfilepath):
+			raise BaseException("file not found")
+		else:
+			vixfilename = checkfilepath
 			print "File %s found" % vixfilename
 			tree = ET.parse(vixfilename)
 			root = tree.getroot()
 			#print root.tag
 			#print root.find('Time').text
 			self.permillis = root.find('EventPeriodInMilliseconds').text
-			if self.channels == 0:
+			if len(root.find('Channels')) > 0:
+				print "setting channels to %s" % len(root.find('Channels'))
 				self.channels = len(root.find('Channels'))
 			self.event_values = root.find('EventValues').text
 			if root.find('Audio') is not None:
-				self.musicfilename = root.find('Audio').get('filename')
+				tempmusicfilename = root.find('Audio').get('filename')
+				print 'Attempting to load music file'
+				self.loadmusic(tempmusicfilename)
 				self.duration = root.find('Audio').get('duration')
 				self.title = root.find('Audio').text
+			if root.find('Profile') is not None:
+				tempprofilename = root.find('Profile').text
+				print 'Attempting to load profile file'
+				self.loadvixprofile(tempprofilename)
 			if root.find('PlugInData') is not None:
 				pass
 				PlugInData = root.find('PlugInData')[0]
-				print PlugInData.get('name')
-				print PlugInData.get('enabled')
-				print PlugInData.get('from')
-				print PlugInData.get('to')
+				print PlugInData.get('name'),
+				print PlugInData.get('enabled'),
+				print PlugInData.get('from'),
+				print PlugInData.get('to'),
 				if PlugInData.get('name') == 'Generic serial':
 					pass
-					print PlugInData.find('Name').text
-					print PlugInData.find('Baud').text
-					print PlugInData.find('Parity').text
-					print PlugInData.find('Data').text
-					print PlugInData.find('Stop').text
+					print PlugInData.find('Name').text,
+					print PlugInData.find('Baud').text,
+					print PlugInData.find('Parity').text,
+					print PlugInData.find('Data').text,
+					print PlugInData.find('Stop').text,
 			#if root.find('Profile') is not None:
 			#	print root.find('Profile').text
 				
 			"""---"""	
 			#self.processdata(self.event_values)
+			print
+			
 	def loadvixprofile(self,vixprofilename):
-		profilefile = readfile(vixprofilename)
-		if not profilefile:
-			raise "Vixen profile file '%s' not found" % vixprofilename
-		#profilefile = "C:\\Users\\BOSCIA\\Portable\\Vixen 2.1.1.0\\profiles\\" + root.find('Profile').text + ".pro"
-		print profilefile
-		if os.path.exists(profilefile):
-			tree = ET.parse(profilefile)
+		if self.basedirpath != '':
+			print "Using base dir path %s" % self.basedirpath
+			checkfilepath = self.basedirpath + '/' + self.defaultprodir + vixprofilename + '.pro'
+		else:
+			print 'base file path not set'
+			checkfilepath = vixprofilename
+		print "loading profile file %s" % checkfilepath
+		if not os.path.exists(checkfilepath):
+			print ("Profile file '%s' not found" % checkfilepath)
+		else:
+			tree = ET.parse(checkfilepath)
 			root = tree.getroot()
 			print root.tag
 			channel_objects = int(len(root.find('ChannelObjects')))
 			print channel_objects
-			if self.channels == 0 and channel_objects > 0:
+			if channel_objects > 0:
 				self.channels = len(root.find('ChannelObjects'))
 				print "setting channels from profile"
 			if root.find('PlugInData') is not None:
@@ -104,11 +131,45 @@ class Vixen(object):
 					#print PlugInData.find('Parity').text
 					#print PlugInData.find('Data').text
 					#print PlugInData.find('Stop').text	
+
+	def loadvixprogram(self,vixprogramname):
+		if self.basedirpath != '':
+			print "Using base dir path %s" % self.basedirpath
+			checkfilepath = self.basedirpath + '/' + self.defaultprodir + vixprogramname
+		else:
+			print 'base file path not set'
+			checkfilepath = vixprogramname
+		print "loading profile file %s" % checkfilepath
+		if not os.path.exists(checkfilepath):
+			print ("Profile file '%s' not found" % checkfilepath)
+		else:
+			tree = ET.parse(checkfilepath)
+			root = tree.getroot()
+			print root.tag
+			sequences = int(len(root.find('Sequence')))
+			print sequences
+			if sequences > 0:
+				print "setting channels from profile"
+			if root.find('Profile') is not None:
+				tempprofilename = root.find('Profile').text
+				print "attempting to load profile"
+				self.loadvixprofile(tempprofilename)
+					
+	def basedir(self,dirname):
+		self.basedirpath = dirname
+		if not os.path.exists(self.basedirpath):
+			raise BaseException("Path %s cannot be found" % self.basedirpath)
+		
 	
 	def loadmusic(self,musicfilename):
-		if os.path.exists(musicfilename):
+		if self.basedirpath != '':
+			checkfilepath = self.basedirpath + '/' + self.defaultmusdir + musicfilename
+		else:
+			checkfilepath = musicfilename
+		print "loading file %s" % checkfilepath
+		if os.path.exists(checkfilepath):
 			self.has_musicfile = True
-			self.musicfilename = musicfilename		
+			self.musicfilename = checkfilepath		
 			#print 'music loaded'
 			print 'loading from loadmusic %s' % self.musicfilename
 			pygame.mixer.init()
@@ -131,11 +192,13 @@ class Vixen(object):
 			#print "music_pos",
 			return pygame.mixer.music.get_pos()
 		else:
-			#print "get_ticks"
-			return pygame.time.get_ticks() - self.ticks_init
+			ticks = pygame.time.get_ticks() - self.ticks_init
+			#print "get_ticks" , ticks, "init", self.ticks_init
+			return ticks
 	
 	def pos_syncwait(self,period,offset=0):
 		x=0
+		#print "get_pos", self.get_pos(), "ticks at per", self.ticks_at_per(int(period)), "period", period
 		while self.get_pos() > 1 and self.get_pos() < self.ticks_at_per(int(period)) + offset:
 			pass
 			
@@ -154,22 +217,25 @@ class Vixen(object):
 		#self.sequence[channel][period] = self.sequence[channel][period]
 	
 	def period_str(self,period):
-	
 		ret = "per: " + str('{:d}'.format(period)) + "/" + str('{:d}'.format(self.periods)) + ": " + ",".join(['{:03d}'.format(ord(self.sequence[x][period])) for x in range(self.channels)])
 		return ret
 		#'{:5d}'.format(period), 
+
+	def period_arr(self,period):
+		ret = [ord(self.sequence[x][period]) for x in range(self.channels)]
+		return ret
 		
 	def processdata(self,datastr,channels=0):
 		self.data = datastr.decode('base64')
 		if channels > 0:
 			self.channels = channels
 		if self.channels == 0:
-			raise "Channels = 0" 
+			raise BaseException("Channels = 0")
 		self.periods = len(self.data)/self.channels
 		print "%s channels, %s periods" % (str(self.channels),str(self.periods))
 		self.sequence = [ self.data[start:start+self.periods] for start in range(0, len(self.data), self.periods) ]
-		print str(self.sequence[0][0])
-		print "hello:" + str(hex(ord(self.sequence[0][0])))		
+		#print str(self.sequence[0][0])
+		#print "hello:" + str(hex(ord(self.sequence[0][0])))		
 		
 """****************************************   TESTING   ****************************************"""
 if __name__ == "__main__":		
