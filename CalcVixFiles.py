64562036f639098e584db87ef57bd07ea9b86b03
BahShaman <mikeboscia@gmail.com>
2014-10-24 22:07:53 -0700

    new file scripts

---
+++ CalcVixFiles.py
@@ -0,0 +1,10 @@
+#CalcVixFiles
+
+where is the current directory (CURDIR)
+
+first look at the seq file passed in.
+	if its a valid file:
+		(meaning its found in the current directory)
+		(check if profile in same dir)
+		(look up one directory)
+	
