f910993cfec9e72b7a25c0e65655e9674636ecea
Mike B <mikeboscia@gmail.com>
2016-02-28 10:50:11 -0800

    Attempting to make work on windows.  May be moved to a test later

--- python-scripts/VixenProgram.py
+++ python-scripts/VixenProgram.py
@@ -1,4 +1,6 @@
 #check files
+import sys
+sys.path.append('../vixen')
 from vixen import Vixen
 from ArduinoSerial import ArduinoSerial
 
@@ -20,7 +22,8 @@ for file in files:
 	print 
 	print file
 	vix = Vixen()
-	vix.basedir('/home/pi/Github/vixen-python/VixenFiles')
+	#vix.basedir('/home/pi/Github/vixen-python/VixenFiles')
+	vix.basedir('../VixenFiles')
 	vix.loadfile(vixfilename)
 	print vix.channels
 	vix.processdata(vix.event_values,vix.channels)
@@ -35,8 +38,8 @@ for file in files:
 		per += 1
 		#print vix.periods, per
 		ard.send(vix.period_arr(per))
-		#print vix.period_str(per)
-		if per % 100 == 0:
-			print '.', 
+		print vix.period_str(per)
+		#if per % 100 == 0:
+			#print '.', 
 	ard.send([0,0,0,0,0,0])
 	vix = None
